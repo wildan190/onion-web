fcaf64f8485e01ab0b6dfc79d6b748964e6e8741
Wildan Belfiore <mikewildan@outlook.com>
2026-04-05 12:56:35 +0700

    add icon package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mitrabawang.id',
-	integrations: [sitemap()],
+	integrations: [sitemap(), icon()],
 });
